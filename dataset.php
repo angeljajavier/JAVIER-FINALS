2e516408fb13444cd2a3ac552255fbf8c84e0b2b
angeljajavier <angeljajavier05@gmail.com>
2022-12-10 16:11:52 +0800

    finalstart

---
+++ dataset.php
@@ -0,0 +1,60 @@
+<?php
+    $products = array(
+        array(
+			'name' 		  => "Ning Yizhuo Top ",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "1550",
+			'photo1'	  => "product1A.jpg",
+			'photo2'	  => "product1B.jpg",
+		),
+		array(
+			'name' 		  => "Karina Cross Top",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "1550",
+			'photo1'	  => "product2A.jpg",
+			'photo2'	  => "product2B.jpg",
+		),
+		array(
+			'name' 		  => "Long Giselle Sleeves",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "2750",
+			'photo1'	  => "product3A.jpg",
+			'photo2'	  => "product3B.jpg",
+		),
+		array(
+			'name' 		  => "Winter Dark Top",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "3600",
+			'photo1'	  => "product4A.jpg",
+			'photo2'	  => "product4B.jpg",
+		),
+		array(
+			'name' 		  => "Joy Button Down",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "1000",
+			'photo1'	  => "product5A.jpg",
+			'photo2'	  => "product5B.jpg",
+		),
+		array(
+			'name' 		  => "White Long Sleeves",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "2800",
+			'photo1'	  => "product6A.jpg",
+			'photo2'	  => "product6B.jpg",
+		),
+		array(
+			'name' 		  => "Acubi Blazer and Shirt",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "3750",
+			'photo1'	  => "product7A.jpg",
+			'photo2'	  => "product7B.jpg",
+		),
+		array(
+			'name' 		  => "Button Down Dress",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "3650",
+			'photo1'	  => "product8A.jpg",
+			'photo2'	  => "product8B.jpg",
+		),																			
+	);
+  ?>
