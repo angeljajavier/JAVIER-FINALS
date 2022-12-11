be56be18eb8b76181eb2499c95f5645701668e2f
angeljajavier <angeljajavier05@gmail.com>
2022-12-11 20:31:52 +0800

    registerr

--- registration.php
+++ registration.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/reg.css">
     <title>User Registration</title>
 </head>
 <body>
@@ -17,35 +20,57 @@
 
 </div>
 
-<div>
-    <form action="registration.php" method="post"></form>
-        <div class = "container">
-            <h1>Registration</h1>
-            <p>Please fill up this form</p>
-            <label for="firstname">First Name</b></label>
-            <input type="text" name="firstname" required>
+<div class="wrapper" style="background-image: url('img/bg-registration-form-1.jpg');">
+			<div class="inner">
+				<div class="image-holder">
+					<img src="img/registration-form-1.jpg" alt="">
+				</div>
+				<form action="">
+					<h3>Registration Form</h3>
+					<div class="form-group">
+						<input type="text" placeholder="First Name" class="form-control">
+						<input type="text" placeholder="Last Name" class="form-control">
+					</div>
+					<div class="form-wrapper">
+						<input type="text" placeholder="Username" class="form-control">
+						<i class="zmdi zmdi-account"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="text" placeholder="Email Address" class="form-control">
+						<i class="zmdi zmdi-email"></i>
+					</div>
+					<div class="form-wrapper">
+						<select name="" id="" class="form-control">
+							<option value="" disabled selected>Gender</option>
+							<option value="male">Male</option>
+							<option value="femal">Female</option>
+							<option value="other">Other</option>
+						</select>
+						<i class="zmdi zmdi-caret-down" style="font-size: 17px"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" placeholder="Password" class="form-control" >
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" placeholder="Confirm Password" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<button>Register
+						<i class="zmdi zmdi-arrow-right"></i>
+					</button>
+                    <a href="login.php"><button>Already have an account? Login</button></a>
+                    <a href="index.php" class="btn btn-primary"> 
+                    <i class="fa fa-shopping-cart"></i> Shop
+        
+                    
+						
+                    
+					</button>
+				</form>
+			</div>
+		</div>
 
-            <label for="lastname">Last Name</b></label>
-            <input type="text" name="lastname" required>
-            
-            <label for="email">Email Address</b></label>
-            <input type="email" name="email" required>
 
-            <label for="phonenumber">Phone Number</b></label>
-            <input type="text" name="phonenumber" required>
-
-            <label for="password">Password</b></label>
-            <input type="password" name="password" required>
-
-            <input type="submit" name="create" value="Sign Up"><br>
-            <a href="index.php" class="btn btn-danger btn-lg"><i class="fa fa-arrow-left"></i> Cancel / Go Back</a>
-
-
-        </div>
-</div>
-
-
-
-    
 </body>
 </html>
